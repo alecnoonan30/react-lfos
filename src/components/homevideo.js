88faf272dc373dfcfda7a03ce87410de8086f022
Alec Noonan <alecnoonan30@gmail.com>
2022-03-20 00:09:02 -0400

    remove vinyl

--- src/components/homevideo.js
+++ src/components/homevideo.js
@@ -6,7 +6,7 @@ import LFOSVIDEO from "../homeVid/LFOSVIDEO.mp4";
 
 export default function Video() {
   return (
-    <video className="homevideo" autoPlay loop muted>
+    <video className="homevideo" autoPlay loop muted playsInline>
       <source src={LFOSVIDEO} type="video/mp4"></source>
     </video>
   );
