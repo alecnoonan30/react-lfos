6d1b265baf38c16f122c83ca3c0ca4efa9ba2a82
Alec Noonan <alecnoonan30@gmail.com>
2022-03-20 01:13:53 -0400

    final commit before launch

--- src/components/homevideo.js
+++ src/components/homevideo.js
@@ -6,7 +6,7 @@ import LFOSVIDEO from "../homeVid/LFOSVIDEO.mp4";
 
 export default function Video() {
   return (
-    <video className="homevideo" autoPlay loop muted defaultMuted>
+    <video className="homevideo" autoPlay loop muted>
       <source src={LFOSVIDEO} type="video/mp4"></source>
     </video>
   );
